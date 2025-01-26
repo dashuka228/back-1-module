5880a532abc9f26ee15749afaa85ef4ff616c3d9
Дарья Лукьянова <lukyanovadasha17@gmail.com>
2025-01-26 23:43:49 +0300

    test

--- back1.js
+++ back1.js
@@ -1,6 +1,8 @@
-import helloFromSubmodule from 'front-sub/submodule.js';
+import helloFromSubmodule from './front-sub/submodule.js';
 	
 function helloFromBack1() {
 	console.log('hello from back1');
 	helloFromSubmodule();
 }
+
+helloFromBack1();
